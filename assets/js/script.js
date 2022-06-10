43579c2337074ad71c2b3abd171d6a8a264a8a44
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2022-06-10 13:16:59 -0300

    alterei as cores

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,12 +16,15 @@ const relogio = setInterval(function time() {
   minutos.textContent = min;
   segundos.textContent = sec;
 
-  if (hr >= 6 && hr < 12) {
+  if (hr >= 5 && hr < 7) {
     document.body.style.background =
-      "linear-gradient(120deg, #2980B9, #6DD5FA, #FFFFFF)";
-  } else if (hr >= 12 && hr <= 18) {
+      "linear-gradient(120deg, #108dc7, #ef8e38)";
+  } else if (hr >= 7 && hr <= 16) {
     document.body.style.background =
-      "linear-gradient(120deg, #ffe259, #ffa751)";
+      "linear-gradient(120deg, #FDC830, #F37335)";
+  } else if (hr >= 16 && hr <= 18) {
+    document.body.style.background =
+      "linear-gradient(120deg, #8A2387, #E94057, #F27121)";
   } else {
     document.body.style.background =
       "linear-gradient(120deg, #0F2027, #203A43, #2c5364)";
